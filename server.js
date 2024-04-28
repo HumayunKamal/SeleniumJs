553d651475b7aa9bba5c0a805e5747504b60e1d8
Humayun Kamal <kamalhumayun320@gmail.com>
2024-04-28 23:06:23 +0500

    proxy configuration added

--- server.js
+++ server.js
@@ -1,15 +1,65 @@
-// builder from selenium
-import { Builder } from "selenium-webdriver";
+// Builder from selenium
+// Capabilities for adding arguments (configuring proxies)
+import { Builder, Capabilities } from "selenium-webdriver";
+// proxy for using proxy methods
+import proxy from "selenium-webdriver/proxy.js";
+// Chrome for Chrome configuration
+import chrome from "selenium-webdriver/chrome.js";
 
 // Path and File System for Exploring directories
 import path from "path";
 import fs from "fs";
+
+// ProxyChain is use to create proxy server
+import proxyChain from "proxy-chain";
 
 // Website Link for scraping
 const websiteLink = "https://myip.ms/"; /* Any link */
 
 const checkWebsite = async (weblink) => {
   // Config Driver
+  // Ip Authentication Config
+  // const driver = new Builder()
+  //   .forBrowser("chrome")
+  //   .setProxy(
+  //     proxy.manual({
+  //       http: "http://domainName:domainPort" /* proxyProviderDomainName:domainPort */,
+  //       https:
+  //         "https://domainName:domainPort" /* proxyProviderDomainName:domainPort */,
+  //     })
+  //   )
+  //   .build();
+
+  // Username and Password Config
+  // const driver = new Builder()
+  //   .forBrowser("chrome")
+  //   .setProxy(
+  //     proxy.manual({
+  //       http: "http://username:password@DomainName:Port" /* username:password@ProxyDomainName:DomainPort */,
+  //       https:
+  //         "https://username:password@DomainName:Port" /*username:password ProxyDomainName:DomainPort */,
+  //     })
+  //   )
+  //   .build();
+
+  // Username and Password Auth with Library
+  // Third party library for proxy authentication
+  // const proxyAgent = await proxyChain.anonymizeProxy(
+  //   "http://username:password@DomainName:DomainPort"
+  // );
+
+  // Config Driver
+  //  const driver = new Builder()
+  //  .forBrowser("chrome")
+  //  .setProxy(
+  //    proxy.manual({
+  //      http: proxyAgent,
+  //      https: proxyAgent,
+  //    })
+  //  )
+  //  .build();
+
+  // Config Driver (without any proxy)
   const driver = new Builder().forBrowser("chrome").build();
 
   //   Maximize the screen
@@ -45,7 +95,7 @@ const checkWebsite = async (weblink) => {
 
   // Close the browser (Prevent memory leaks)
   // though while testing you can comment it out or use driver.sleep(time in milisecond)
-  await driver.quit();
+  // await driver.quit();
 };
 
 checkWebsite(websiteLink);
