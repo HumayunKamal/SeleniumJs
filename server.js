048c0d2489e26ec3730bf3ad25db33cdb2eb4bc8
Humayun Kamal <kamalhumayun320@gmail.com>
2024-04-28 23:08:45 +0500

    rerun function added now scrapper will rerun after specific time

--- server.js
+++ server.js
@@ -10,22 +10,33 @@ import chrome from "selenium-webdriver/chrome.js";
 import path from "path";
 import fs from "fs";
 
+// For importing values from .env
+import dotenv from "dotenv";
+dotenv.config();
+
 // ProxyChain is use to create proxy server
 import proxyChain from "proxy-chain";
 
-// Website Link for scraping
-const websiteLink = "https://myip.ms/"; /* Any link */
+// nodemailder for sending email
+import nodemailer from "nodemailer";
 
-const checkWebsite = async (weblink) => {
+// const from .env
+const websiteLink = process.env.WEBSITE_LINK; /* Any website link */
+const rerunWait = process.env.RERUN_TIME;
+const proxyUsername = process.env.DOMAIN_USERNAME;
+const proxyPassword = process.env.DOMAIN_PASSWORD;
+const proxyDomain = process.env.PROXYDOMAIN;
+const proxyPort = process.env.PROXYPORT;
+
+const checkWebsite = async (parentDir) => {
   // Config Driver
   // Ip Authentication Config
   // const driver = new Builder()
   //   .forBrowser("chrome")
   //   .setProxy(
   //     proxy.manual({
-  //       http: "http://domainName:domainPort" /* proxyProviderDomainName:domainPort */,
-  //       https:
-  //         "https://domainName:domainPort" /* proxyProviderDomainName:domainPort */,
+  //       http: `${proxyDomain}:${proxyPort}` /* proxyProviderDomainName:domainPort */,
+  //       https: `${proxyDomain}:${proxyPort}` /* proxyProviderDomainName:domainPort */,
   //     })
   //   )
   //   .build();
@@ -35,9 +46,8 @@ const checkWebsite = async (weblink) => {
   //   .forBrowser("chrome")
   //   .setProxy(
   //     proxy.manual({
-  //       http: "http://username:password@DomainName:Port" /* username:password@ProxyDomainName:DomainPort */,
-  //       https:
-  //         "https://username:password@DomainName:Port" /*username:password ProxyDomainName:DomainPort */,
+  //       http: `${proxyUsername}:${proxyPassword}@${proxyDomain}:${proxyPort}` /* username:password@ProxyDomainName:DomainPort */,
+  //       https: `${proxyUsername}:${proxyPassword}@${proxyDomain}:${proxyPort}` /*username:password ProxyDomainName:DomainPort */,
   //     })
   //   )
   //   .build();
@@ -45,39 +55,40 @@ const checkWebsite = async (weblink) => {
   // Username and Password Auth with Library
   // Third party library for proxy authentication
   // const proxyAgent = await proxyChain.anonymizeProxy(
-  //   "http://username:password@DomainName:DomainPort"
+  //   `http://${proxyUsername}:${proxyPassword}@${proxyDomain}:${proxyPort}`
   // );
 
-  // Config Driver
-  //  const driver = new Builder()
-  //  .forBrowser("chrome")
-  //  .setProxy(
-  //    proxy.manual({
-  //      http: proxyAgent,
-  //      https: proxyAgent,
-  //    })
-  //  )
-  //  .build();
+  // // Config Driver
+  // const driver = new Builder()
+  //   .forBrowser("chrome")
+  //   .setProxy(
+  //     proxy.manual({
+  //       http: proxyAgent,
+  //       https: proxyAgent,
+  //     })
+  //   )
+  //   .build();
+
+  // Chrome Config
+  const chromeOptions = new chrome.Options();
+  // chromeOptions.addArguments(
+  //   "--headless"
+  // ); /* Run scrapper headlessly like won't open chrome window but will run in background */
 
   // Config Driver (without any proxy)
-  const driver = new Builder().forBrowser("chrome").build();
+  const driver = new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(chromeOptions)
+    .build();
 
   //   Maximize the screen
   driver.manage().window().maximize();
 
   //   Get Website
-  await driver.get(weblink);
+  await driver.get(websiteLink);
 
   // Selenium Code for Scrapping
   // Xpath or CSS Selection
-
-  // Create directory where scraper will save screenshots
-  const directory = "screenshots"; /* Folder Name */
-
-  // Create Folder (recursive won't give error if the directory already exists)
-  fs.mkdirSync(directory, {
-    recursive: true,
-  });
 
   // Get the Current time and use it for fileName
   const currentTime = new Date();
@@ -86,16 +97,36 @@ const checkWebsite = async (weblink) => {
     .replace(/[/,: ]/g, "_"); /* replace (/,:, ) with (_) */
 
   // Combine the directory path and the formatted time to create the full path for the screenshot
-  const screenshotPath = path.join(directory, `${formattedTime}.png`);
+  const screenshotPath = path.join(parentDir, `${formattedTime}.png`);
 
   // Take Screenshot and Save
   await driver.takeScreenshot().then((data) => {
     fs.writeFileSync(screenshotPath, data, "base64");
   });
+  console.log("ScreenShot Taken and saved");
 
   // Close the browser (Prevent memory leaks)
   // though while testing you can comment it out or use driver.sleep(time in milisecond)
-  // await driver.quit();
+  await driver.quit();
 };
 
-checkWebsite(websiteLink);
+// Create directory where scraper will save screenshots
+const parentDirectory = "screenshots"; /* Folder Name */
+
+// Create Folder (recursive won't give error if the directory already exists)
+fs.mkdirSync(parentDirectory, { recursive: true });
+
+// Function to run the website check and take screenshots
+async function runWebsiteCheck() {
+  try {
+    await checkWebsite(parentDirectory);
+  } catch (error) {
+    console.error(`Error occurred: ${error.message}`);
+  }
+}
+
+// Run the website and check immediately
+runWebsiteCheck();
+
+// Set interval to run the website check every (rerunWait in seconds) minutes
+setInterval(runWebsiteCheck, rerunWait * 1000); // rerunWait is in seconds, so multiply by 1000 to convert to milliseconds
